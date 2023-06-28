5fbeeb9dcdbbfb81962da45846ab13285121a1a3
ymarquina <yeseniamrqna@gmail.com>
2023-06-28 16:29:56 -0700

    Add functionality and fix bugs.

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-let firstColor = document.querySelector(".first-color");
-let secondColor = document.querySelector(".second-color");
+let colorInput = document.querySelectorAll("input");
 let body = document.querySelector("body");
+let leftColor = document.querySelector(".color1");
+let rightColor = document.querySelector(".color2");
 
 
 function changeBackground(color, pos){
     if(pos === 'left'){
-    body.style.backgroundImage = `linear-gradient(to right, ${color} , yellow)`
+        body.style.backgroundImage = `linear-gradient(to right, ${color} , ${rightColor.innerHTML})`
+        leftColor.innerHTML = color;
     }else{
-        body.style.backgroundImage = `linear-gradient(to right, ${color} , yellow)`
+        body.style.backgroundImage = `linear-gradient(to right, ${leftColor.innerHTML} , ${color})`
+        rightColor.innerHTML = color;
     }
+
 }
 
-
-
-firstColor.addEventListener('input', (e) => {
-    console.log(e.target.value);
-    changeBackground(e.target.value);
-})
-
-secondColor.addEventListener('input', (e) => {
-
+colorInput.forEach(color => {
+    color.addEventListener('input', (e) => {
+        let newColor = e.target.value
+        let side = e.target.dataset.side
+        changeBackground(newColor, side);
+    })
 })
