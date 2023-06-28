6a3292052dfb0fb81c475c1111f14cc1bcbbd9c7
ymarquina <yeseniamrqna@gmail.com>
2023-06-27 22:37:19 -0700

    Add basic html elements and create input functions

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("test");
+let firstColor = document.querySelector(".first-color");
+let secondColor = document.querySelector(".second-color");
+let body = document.querySelector("body");
+
+
+function changeBackground(color, pos){
+    if(pos === 'left'){
+    body.style.backgroundImage = `linear-gradient(to right, ${color} , yellow)`
+    }else{
+        body.style.backgroundImage = `linear-gradient(to right, ${color} , yellow)`
+    }
+}
+
+
+
+firstColor.addEventListener('input', (e) => {
+    console.log(e.target.value);
+    changeBackground(e.target.value);
+})
+
+secondColor.addEventListener('input', (e) => {
+
+})
